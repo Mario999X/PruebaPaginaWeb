745c4c178e22b705e71dfb4117cf75d118cc0e88
Mario <mariogr901@gmail.com>
2022-11-01 23:14:16 +0100

    Pequeño arreglo + comprobacion.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -25,6 +25,7 @@ fun main() {
 
         pB = ProcessBuilder("cmd.exe", "/c", "cd $pathJar & java -jar PruebaPaginaWeb-1.0.jar $url").start()
         reader = pB.inputStream.bufferedReader().lineSequence().joinToString("\n")
+        //println(reader)
 
         println("HTML leido correctamente")
         file.writeText(reader)
@@ -34,8 +35,9 @@ fun main() {
 
         pB = ProcessBuilder("bash", "-c", "cd $pathJar && java -jar PruebaPaginaWeb-1.0.jar $url").start()
         reader = pB.inputStream.bufferedReader().lineSequence().joinToString("\n")
+        println(reader)
 
-        println("HTML leido correctamente")
+        //println("HTML leido correctamente")
         file.writeText(reader)
         println("HTML escrito correctamente en $pathData")
     }
